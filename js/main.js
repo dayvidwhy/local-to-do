bd91e9c2a72174657fb84283e503c5a1a6a35fff
dayvidwhy <dayvidwhy@gmail.com>
2017-01-27 22:45:48 +1000

    Turn into module.

--- js/main.js
+++ js/main.js
@@ -1,64 +1,72 @@
 /*
-    To Do List using Local Storage
-    Stores a stringified version of the object we keep track of.
+To-Do using Module Pattern
+Version: v0.1.0
+Author: David Young
 */
 
-// methods
-var toDo = {
-    // check if storage available.
-    available: function() {
-        if (!this.supported) {
-            if (typeof(Storage) !== "undefined") {
-                this.supported = true;
-            } else {
-                this.supported = false;
-            }
-            return this.supported;
-        }
-        return this.supported; // we already checked before
-    },
+;var SimpleToDo = (function(window, document, $, undefined) {
 
-    // gets list of To-Do's from local storage
-    fetch: function() {
+    // Where we store our To-Do list.
+    var list = [];
+
+    /* 
+    * Check if storage available.
+    */
+    function localStorageAvailable() {
+        return typeof(Storage) !== 'undefined';
+    }
+
+    /*
+    * Gets list of To-Do's from local storage
+    */
+    function fetch() {
         var retrievedObject = localStorage.getItem('toDoList');
         if (retrievedObject === null) {
-            this.list = [];
+            list = [];
         } else {
-            this.list = JSON.parse(retrievedObject);
+            list = JSON.parse(retrievedObject);
         }
-    },
+    }
 
-    // saves list of to-do's to local storage
-    save: function() {
-        localStorage.setItem('toDoList', JSON.stringify(this.list));
-    },
+    /*
+    * Saves list of to-do's to local storage
+    */
+    function save() {
+        localStorage.setItem('toDoList', JSON.stringify(list));
+    }
 
-    // removes an item from local storage
-    remove: function(date, text) {
-        for (var i = 0; i < this.list.length; i++) {
-            if (this.list[i].date === date
-                    && this.list[i].text === text) {
-                this.list.splice(i, 1);
+    /* 
+    * Removes an item from local storage
+    */
+    function removeToDo(date, text) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].date === date &&
+                    list[i].text === text) {
+                list.splice(i, 1);
             break;
             }
         }
-        this.save();
-    },
+        save();
+    }
 
-    // toggles an items active state
-    toggle: function(date, text) {
-        for (var i = 0; i < this.list.length; i++) {
-            if (this.list[i].date === date
-                    && this.list[i].text === text) {
-                this.list[i].active = !this.list[i].active;
+    /* 
+    * Toggles an items active state.
+    */
+    function toggle(date, text) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].date === date &&
+                    list[i].text === text) {
+                list[i].active = !list[i].active;
             break;
             }
         }
-        this.save();
-    },
+        save();
+    }
 
-    // Prints out a Todo on the page.
-    printToDo: function(date, text, active) {
+    /* 
+    * Prints out a Todo on the page.
+    */
+    function printToDo(date, text, active) {
         var panel = document.createElement('div');
         
         var heading = document.createElement('div');
@@ -88,8 +96,8 @@ var toDo = {
             var panelOwner = this.parentElement.parentElement;
             panelOwner.classList.remove('show');
             panelOwner.classList.add('hide');
-            toDo.toggleActiveClass(this);
-            toDo.toggle(date, text);
+            toggleActiveClass(this);
+            toggle(date, text);
         });
 
         // create button that lets the panel be deleted
@@ -98,7 +106,7 @@ var toDo = {
         delButton.innerHTML = 'Delete';
         delButton.addEventListener('click', function() {
             this.parentElement.parentElement.outerHTML = "";
-            toDo.remove(date, text);
+            removeToDo(date, text);
         });
 
         // Add to the DOM
@@ -108,80 +116,96 @@ var toDo = {
         panel.appendChild(heading);
         panel.appendChild(body);
         $('#output').appendChild(panel);
-    },
+    }
 
-    clickHandlers: function() {
-        // Add click handlers
+    /*
+    * When the user clicks revert or done.
+    */
+    function stateEvent(e) {
+        if (this.classList.contains('active')) {
+            return;
+        }
+        var active, inactive;
+        this.classList.add('active');
+        if (this.innerHTML === "Active") { //pretty poor check
+            // they clicked the active button
+            $("#done").classList.remove('active');
+            inactive = $(".panel-to-do");
+            active = $(".panel-done");
+        } else {
+            // they clicked the done button
+            $("#active").classList.remove('active');
+            inactive = $(".panel-done");
+            active = $(".panel-to-do");
+        }
+        for (var activeIndex = 0; activeIndex < active.length; activeIndex++) {
+            active[activeIndex].classList.add('hide');
+            active[activeIndex].classList.remove('show');
+        }
+        for (var inIndex = 0; inIndex < inactive.length; inIndex++) {
+            inactive[inIndex].classList.add('show');
+            inactive[inIndex].classList.remove('hide');
+        }
+    }
+
+    /*
+    * Apply the click handlers.
+    */
+    function clickHandlers() {
         var states = $('.state-buttons');
         for (var i = 0; i < states.length; i++) {
-            states[i].addEventListener('click', function() {
-                if (this.classList.contains('active')) {
-                    return;
-                }
-                var active, inactive;
-                this.classList.add('active');
-                if (this.innerHTML === "Active") { //pretty poor check
-                    // they clicked the active button
-                    $("#done").classList.remove('active');
-                    inactive = $(".panel-to-do");
-                    active = $(".panel-done");
-                } else {
-                    // they clicked the done button
-                    $("#active").classList.remove('active');
-                    inactive = $(".panel-done");
-                    active = $(".panel-to-do");
-                }
-                for (var i = 0; i < active.length; i++) {
-                    active[i].classList.add('hide');
-                    active[i].classList.remove('show');
-                }
-                for (var i = 0; i < inactive.length; i++) {
-                    inactive[i].classList.add('show');
-                    inactive[i].classList.remove('hide');
-                }
-            });
+            states[i].addEventListener('click', stateEvent);
         }
         // Add a to-do panel to the page
-        $('#submit').addEventListener('click', this.newToDo);
+        $('#submit').addEventListener('click', newToDo);
 
         // bind this context
-        $('input')[0].addEventListener('keypress', this.keyCallback.bind(this));
+        $('input')[0].addEventListener('keypress', keyCallback);
 
         $("#clear").addEventListener('click', function() {
-            toDo.clear();
+            clearToDo();
             var all = $(".panel");
             for (var i = 0; i < all.length; i++) {
                 all[i].outerHTML = ""; // purge panels
             }
         });
-    },
+    }
 
-    keyCallback: function(e) {
+    /*
+    * Enter key support.
+    */
+    function keyCallback(e) {
         var key = e.which || e.keyCode;
         if (key === 13) { // listen for enter key
-          this.newToDo();
+          newToDo();
         }
-    },
+    }
 
-    newToDo: function() {
+    /*
+    * We want to add a new ToDo.
+    */
+    function newToDo() {
         var text = $('input')[0].value;
         if (text.length === 0) {
             return;
         }
         var date = new Date();
         var titleDate = date.toDateString() + ' ' + toRelativeTime(date.getHours(), date.getMinutes());
-        toDo.printToDo(titleDate, text, true);
+        printToDo(titleDate, text, true);
         $('input')[0].value = "";
-        toDo.list.push({
+        list.push({
             'date'  : titleDate,
             'text'  : text,
             'active': true
         });
         $("#active").click();
-        toDo.save();
-    },
+        save();
+    }
 
-    toggleActiveClass: function(button) {
+    /*
+    * Swap the active class.
+    */
+    function toggleActiveClass(button) {
         var panelElement = button.parentElement.parentElement;
         var classes = panelElement.classList;
         if (panelElement.classList.contains('panel-to-do')) {
@@ -193,41 +217,51 @@ var toDo = {
             panelElement.classList.remove('panel-done');
             button.innerHTML = 'Done';
         }
-    },
-
-    // todo- clear button
-    clear: function() {
-        this.list = [];
-        this.save();
-    },
-
-    // setup list
-    init: function() {
-        if (!this.available) {
-            $('#output').innerHTML = 'Please update your browser to use this page.';
-            $('#output').style.fontSize = "24px";
-            $('#output').style.color = "#a94442";
-            return;
-        }
-        this.fetch();
-        var length = this.list.length;
-        if (length > 0) {
-            for (var i = 0; i < length; i++) {
-                this.printToDo(this.list[i].date, this.list[i].text,
-                        this.list[i].active);
-            }
-        }
-        this.clickHandlers();
     }
-}
 
-toDo.init(); // Start
+    /*
+    * Clear To-Do's
+    */
+    function clearToDo() {
+        list = [];
+        save();
+    }
+
+    /*
+    * Return the public API.
+    */
+    return {
+        init: function() {
+            if (!localStorageAvailable()) {
+                $('#output').innerHTML = 'Please update your browser to use this page.';
+                $('#output').style.fontSize = "24px";
+                $('#output').style.color = "#a94442";
+                return;
+            }
+            fetch();
+            var length = list.length;
+            if (length > 0) {
+                for (var i = 0; i < length; i++) {
+                    
+                    // print each to-do
+                    printToDo(
+                        list[i].date, 
+                        list[i].text, 
+                        list[i].active
+                    );
+                }
+            }
+            clickHandlers();
+        }
+    };
+})(window, document, documentQuery);
+
+SimpleToDo.init(); // Start
 
 /*
-    cheesy selector
-    works for #id, .class and <tag>
+* Works for #id, .class and <tag>
 */
-function $(element) {
+function documentQuery(element) {
     if (element[0] === '#') {
         return document.getElementById(element.slice(1, element.length));
     }
@@ -235,7 +269,7 @@ function $(element) {
 }
 
 /*
-    Turns 24 hour time into 12 hour time.
+* Turns 24 hour time into 12 hour time.
 */
 function toRelativeTime(hour, minutes) {
     if (minutes < 10) {
@@ -243,7 +277,7 @@ function toRelativeTime(hour, minutes) {
     }
     if (hour > 12) {
         hour -= 12;
-        return hour + ':' + minutes + 'pm'
+        return hour + ':' + minutes + 'pm';
     }
     if (hour === 0) {
         return '12:' + minutes + 'am';
