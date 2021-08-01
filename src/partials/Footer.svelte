675ff0310f4e023a98314cd995362f2aa503f45b
dayvidwhy <dayvidwhy@gmail.com>
2021-08-01 22:56:17 +1000

    Split the app into a few main sections.

---
+++ src/partials/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <p>
+        Find <a href="https://github.com/dayvidwhy/local-to-do/">source</a> and me on <a href="https://github.com/dayvidwhy">Github</a>.
+    </p>
+</footer>
+
+<style>
+    footer > p {
+        text-align: center;
+    }
+</style>
