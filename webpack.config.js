c70e911678e7452349bc47946e0a4a633cb352ff
dayvidwhy <dayvidwhy@gmail.com>
2021-08-09 18:18:04 +1000

    Remove cross-env, can pass flags to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,66 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-const mode = process.env.NODE_ENV || "development";
-const prod = mode === "production";
-
-module.exports = {
-    entry: {
-        "build/bundle": ["./src/main.js"]
-    },
-    resolve: {
-        alias: {
-            svelte: path.dirname(require.resolve("svelte/package.json"))
+module.exports = (env, argv) => {
+    const prod = argv.mode === "production";
+    return {
+        entry: {
+            "build/bundle": ["./src/main.js"]
         },
-        extensions: [".mjs", ".js", ".svelte"],
-        mainFields: ["svelte", "browser", "module", "main"]
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "[name].js",
-        chunkFilename: "[name].[id].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.svelte$/,
-                use: {
-                    loader: "svelte-loader",
-                    options: {
-                        compilerOptions: {
-                            dev: !prod
-                        },
-                        emitCss: prod,
-                        hotReload: !prod
+        resolve: {
+            alias: {
+                svelte: path.dirname(require.resolve("svelte/package.json"))
+            },
+            extensions: [".mjs", ".js", ".svelte"],
+            mainFields: ["svelte", "browser", "module", "main"]
+        },
+        output: {
+            path: path.join(__dirname, "dist"),
+            filename: "[name].js",
+            chunkFilename: "[name].[id].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.svelte$/,
+                    use: {
+                        loader: "svelte-loader",
+                        options: {
+                            compilerOptions: {
+                                dev: !prod
+                            },
+                            emitCss: prod,
+                            hotReload: !prod
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader"
+                    ]
+                },
+                {
+                    // required to prevent errors from Svelte on Webpack 5+
+                    test: /node_modules\/svelte\/.*\.mjs$/,
+                    resolve: {
+                        fullySpecified: false
                     }
                 }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
-            {
-                // required to prevent errors from Svelte on Webpack 5+
-                test: /node_modules\/svelte\/.*\.mjs$/,
-                resolve: {
-                    fullySpecified: false
-                }
-            }
-        ]
-    },
-    mode,
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        }),
-        new htmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html")
-        })
-    ],
-    devtool: prod ? false : "source-map",
-    devServer: {
-        hot: true
-    }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            }),
+            new htmlWebpackPlugin({
+                template: path.resolve(__dirname, "public", "index.html")
+            })
+        ],
+        devtool: prod ? false : "source-map",
+        devServer: {
+            hot: true
+        }
+    };
 };
